e28a76076a77e93e5260ff20468b5f61651a0de1
rsco <RaquelLee@users.noreply.github.com>
2021-03-15 18:16:01 -0400

    relational methods updated

--- models/index.js
+++ models/index.js
@@ -6,24 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+  foreignKey: 'id',
 })
 // Categories have many Products
 Category.hasMany(Product, {
-
+  foreignKey: 'id',
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-  }
+  },
+  as: 'products_tag'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-  }
+  },
+  as: 'tags_product'
+
 })
 
 module.exports = {
