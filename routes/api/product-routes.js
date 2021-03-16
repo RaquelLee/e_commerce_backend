c7e5ebcfa8f38f16601b52e20f87ffa905cde305
rsco <RaquelLee@users.noreply.github.com>
2021-03-16 09:12:11 -0400

    product routes updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,17 +4,14 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   try {
-    const products = await Product.findAll();
+    const products = await Product.findAll({Category, Tag});
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Category and Tag data
-  include: [{ model: Category, Tag}]
-
 });
 
   // find a single product by its `id`
